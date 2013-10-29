7a51a8e5fb817ad9869c6d86401ed2b900a18573
Jari Bakken <jari.bakken@gmail.com>
2013-10-29 11:17:44 +0100

    functional

--- oppgaver/src/main/scala/scalakurs/functional/Functional.scala
+++ oppgaver/src/main/scala/scalakurs/functional/Functional.scala
@@ -18,14 +18,21 @@ object Functional {
   // factorial = fakultet (5! = 5 * 4 * 3 * 2 * 1)
   def factorial(n: Int): Int = {
     def fac0(n: Int, acc: Int): Int = {
-      if (n <= 0) ???
-      else ???
+      if (n <= 0) 1
+      else n * fac0(n - 1, acc)
     }
     fac0(n, 1)
   }
 
   // hvis det er vanskelig å lage en indre metode, slik at den blir tail recursive,
   // kan du forsøke lineær rekursjon
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    def fib0(x: Int, a: Int, b: Int): Int = {
+      if (x <= 1) a
+      else fib0(x - 1, b, a + b)
+    }
+
+    fib0(n, 0, 1)
+  }
 
 }
