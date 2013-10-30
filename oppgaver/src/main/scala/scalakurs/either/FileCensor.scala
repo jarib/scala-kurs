ac045a83bca5980d6762c0a5615cd4e2ed9fe6b7
Jari Bakken <jari.bakken@gmail.com>
2013-10-30 13:32:34 +0100

    Either

--- oppgaver/src/main/scala/scalakurs/either/FileCensor.scala
+++ oppgaver/src/main/scala/scalakurs/either/FileCensor.scala
@@ -1,6 +1,7 @@
 package scalakurs.either
 
 import java.io.File
+import scala.io.Source
 
 
 object FileCensor {
@@ -9,15 +10,22 @@ object FileCensor {
 
   // projection should contain errorMessage if file name contains the wrong words
   // Hint: scala.io.Source.fromFile(file)
-  def fetchUncensoredContent(file: File): Either[String, Seq[String]] = ???
+  def fetchUncensoredContent(file: File): Either[String, Seq[String]] = {
+    if (file.getPath.contains("scala"))
+      Left(errorMessage)
+    else
+      Right(Source.fromFile(file).getLines().toSeq)
+  }
 
   // add the text expected from the tests to each of the projections
   // Hint: either.fold
-  def formatResponse(response: Either[String, Seq[String]]): String = ???
+  def formatResponse(response: Either[String, Seq[String]]): String = {
+    response.fold("Error:\n" + _, "Response:\n" + _.mkString("\n"))
+  }
 
   // Syntax for ignoring case in regex is "(?i)expr"
-  def censorLine(line: String): String = ???
+  def censorLine(line: String): String = line.replaceAll("(?i)scala", "*** CENSORED ***")
 
   // reuse where practical. remember the collection functions from earlier.
-  def fetchCensoredContent(file: File): Either[String, Seq[String]] = ???
+  def fetchCensoredContent(file: File): Either[String, Seq[String]] = fetchUncensoredContent(file).right.map(_.map(censorLine))
 }
--- oppgaver/src/main/scala/scalakurs/option/OptionTree.scala
+++ oppgaver/src/main/scala/scalakurs/option/OptionTree.scala
@@ -7,18 +7,7 @@ class Tree {
     case Leaf(value) => Option(value)
   }
   lazy val mkString: String = ???
-  lazy val nrOfEdges: Int = {
-    @tailrec
-    def inner(count: Int, list: List[Tree]): Int = {
-//      list.headOption match {
-//        case Some(leaf: Leaf) => inner(count, list.tail)
-//        case Some(Node(left, right)) => inner(blaaaaaaaah)
-//        case _ => count
-//      }
-    }
-
-    inner(0, this :: Nil)
-  }
+  lazy val nrOfEdges: Int = ???
 }
 
 case class Node(left: Option[Tree], right: Option[Tree]) extends Tree
