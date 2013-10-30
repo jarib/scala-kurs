2308b60c659846a0f11d56d5b87eb7b76108ba8b
Jari Bakken <jari.bakken@gmail.com>
2013-10-30 14:00:24 +0100

    OptionTree

--- oppgaver/src/main/scala/scalakurs/option/OptionTree.scala
+++ oppgaver/src/main/scala/scalakurs/option/OptionTree.scala
@@ -6,8 +6,31 @@ class Tree {
   lazy val rootValue: Option[String] = this match {
     case Leaf(value) => Option(value)
   }
-  lazy val mkString: String = ???
-  lazy val nrOfEdges: Int = ???
+  lazy val mkString: String = {
+    @tailrec
+    def getValues(str: String, list: List[Tree]): String = list.headOption match {
+      case Some(Leaf(value)) => getValues(str + value, list.tail)
+      case Some(Node(left, right)) => getValues(str, List(left, right).flatten ++ list.tail)
+      case _ => str
+    }
+
+    getValues("", List(this))
+  }
+
+
+  lazy val nrOfEdges: Int = {
+    @tailrec
+    def countEdges(count: Int, list: List[Tree]): Int = list.headOption match {
+      case Some(Leaf(value)) => countEdges(count, list.tail)
+      case Some(Node(left, right)) => {
+        val children = List(left, right).flatten
+        countEdges(count + children.size, List(children, list.tail).flatten)
+      }
+      case _ => count
+    }
+
+    countEdges(0, List(this))
+  }
 }
 
 case class Node(left: Option[Tree], right: Option[Tree]) extends Tree
