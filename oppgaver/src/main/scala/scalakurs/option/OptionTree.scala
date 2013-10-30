82b94e9fb08b297e5634d23b0751c2e115e8b7eb
Jari Bakken <jari.bakken@gmail.com>
2013-10-30 12:57:58 +0100

    meh

--- oppgaver/src/main/scala/scalakurs/option/OptionTree.scala
+++ oppgaver/src/main/scala/scalakurs/option/OptionTree.scala
@@ -1,9 +1,24 @@
 package scalakurs.option
 
+import scala.annotation.tailrec
+
 class Tree {
-  lazy val rootValue: Option[String] = ???
+  lazy val rootValue: Option[String] = this match {
+    case Leaf(value) => Option(value)
+  }
   lazy val mkString: String = ???
-  lazy val nrOfEdges: Int = ???
+  lazy val nrOfEdges: Int = {
+    @tailrec
+    def inner(count: Int, list: List[Tree]): Int = {
+//      list.headOption match {
+//        case Some(leaf: Leaf) => inner(count, list.tail)
+//        case Some(Node(left, right)) => inner(blaaaaaaaah)
+//        case _ => count
+//      }
+    }
+
+    inner(0, this :: Nil)
+  }
 }
 
 case class Node(left: Option[Tree], right: Option[Tree]) extends Tree
